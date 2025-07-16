c9dc82eaa84f9caba60ca7857db02b10e5bf1d73
Jean Alvarez <219008423+kenzul@users.noreply.github.com>
2025-07-16 10:33:48 -0500

    Play individual round

--- script.js
+++ script.js
@@ -15,3 +15,19 @@ const getHumanChoice = () => {
     } while (!deck.includes(choice));
     return choice;
 }
+
+const playRound = (humanChoice, computerChoice) => {
+    const humanLower = humanChoice.toLowerCase();
+    const computerLower = computerChoice.toLowerCase();
+    if (humanLower === computerLower) {
+        return "No one wins! It is a draw!";
+    } else if (deck.indexOf(humanChoice) < deck.indexOf(computerChoice)) {
+        return declareWinner(1, computerChoice, humanChoice);
+    } else {
+        return declareWinner(0, humanChoice, computerChoice);
+    }
+}
+
+const declareWinner = (side, winnerChoice, loserChoice,) => {
+    return `${side === 0 ? "Human" : "Computer"} wins! ${winnerChoice} defeats ${loserChoice}`;
+}
